624cb6176d71509c32ec1e0247366892c4d3268a
kevin <kevin@2777166795@qq.com>
2023-04-06 20:26:08 +0800

    second commit

---
+++ src/main/java/com/zhang/git/GitTest.java
@@ -0,0 +1,8 @@
+package com.zhang.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hjadwak");
+        System.out.println("dawdwad");
+    }
+}
